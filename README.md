1cb5497567745b118dac373d47a27c809ec8dcf8
Matt Lubel <matt_lubel@hotmail.com>
2016-11-30 23:49:16 +0000

    detabbed and tweaked slightly

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # multiplayer RPS (rock, paper, scissors) game simulations
--------------------
 This python script calculates the expected number of rounds required to find a winner from
 an RPS game of (a user inputted) n players. It then runs an inputted number of simulations and reports
 back the mean number of rounds required.
 
-##### Motivations
--------------------
+### Motivations
 I used to play multiplayer RPS often, to find a single/multiple winner(s)/loser(s) in a variety
 of situations. (If you've never played with > 2 players before, maybe have a brief think 
 about how the game works with more players, w.r.t. draws/stalemates). The game can go on a while with
@@ -13,8 +11,10 @@ lots of players (I must've played with around 12 people before), and I was inter
 the average number of rounds required to find a winner. I worked it out mathematically and 
 then made this script to test the result I had derived.
 
-##### Planned Improvements
--------------------
+### Planned Improvements
 - add mathematical reasoning to README
 - make (2-player) RPS robot
 - simulate multiplayer RPS game for (exciting??!) random picking app (cf. tap roulette)
+
+### Collaboration
+Any contributions/comments are welcome!
--- RPS.py
+++ RPS.py
@@ -3,82 +3,82 @@ import random
 
 # define n choose r
 def choose(n ,r):
-	return math.factorial(n) / (math.factorial(n-r) * math.factorial(r))
+    return math.factorial(n) / (math.factorial(n-r) * math.factorial(r))
 
 def expected(n):
-	""" 
-	Returns the expected number of rounds required to find a winner in an RPS game with
-	n players.
-	"""	 
-	list = [0,0,1.5]
-	i = 2
-	while i < n :
-		# calculate i+1 value
-		sum = 0
-		for j in range(2, i+1): 
-			# loop through list
-			sum += choose(i+1,j) * list[j]
-		list.append( (3**(i) + sum) * ( 1.0/(2**(i+1) - 2) ) )
-		i += 1	
-	return list[int(n)]
+    """ 
+    Returns the expected number of rounds required to find a winner in an RPS game with
+    n players.
+    """  
+    list = [0,0,1.5]
+    i = 2
+    while i < n :
+        # calculate i+1 value
+        sum = 0
+        for j in range(2, i+1): 
+            # loop through list
+            sum += choose(i+1,j) * list[j]
+        list.append( (3**(i) + sum) * ( 1.0/(2**(i+1) - 2) ) )
+        i += 1  
+    return list[int(n)]
 
 def play(n):
-	"""
-	returns the number of rounds required to find the winner in a random simulation of
-	a game starting with n players. 
-	"""
-	ret = 0
-	if n == 1:
-		return ret
-	else:
-		ret += 1
-		# choices is list of n choices of 0, 1 or 2 (corresponding arbitrarily to R, P, S)
-		choices = [ random.randint(0,2) for _ in range(int(n)) ]
-		# stalemate if all 3 options appear or only 1 does
-		while (0 in choices) + (1 in choices) + (2 in choices) != 2:
-			ret += 1
-			choices = [ random.randint(0,2) for _ in range(int(n)) ]
-		# can pick random winner out of the 2 which appear
-		# i.e. randomly assign 0, 1, 2 to R, P, S
-		# either 1 or 2 for indexing of sorted tally array
-		winner_index = random.randint(1,2)
-		new_n = sorted([choices.count(x) for x in range(3)])[winner_index]
-		return ret + play(new_n)
+    """
+    returns the number of rounds required to find the winner in a random simulation of
+    a game starting with n players. 
+    """
+    ret = 0
+    if n == 1:
+        return ret
+    else:
+        ret += 1
+        # choices is list of n choices of 0, 1 or 2 (corresponding arbitrarily to R, P, S)
+        choices = [ random.randint(0,2) for _ in range(int(n)) ]
+        # stalemate if all 3 options appear or only 1 does
+        while (0 in choices) + (1 in choices) + (2 in choices) != 2:
+            ret += 1
+            choices = [ random.randint(0,2) for _ in range(int(n)) ]
+        # can pick random winner out of the 2 which appear
+        # i.e. randomly assign 0, 1, 2 to R, P, S
+        # either 1 or 2 for indexing of sorted tally array
+        winner_index = random.randint(1,2)
+        new_n = sorted([choices.count(x) for x in range(3)])[winner_index]
+        return ret + play(new_n)
 
-def recursive_play(n):	
-	"""
-	returns the number of rounds required to find the winner in a random simulation of
-	a game starting with n players. Uses recursion though so doesn't work for high n.
-	"""
-	if n==1:
-		return 0	
-	else:
-		list = []
-		for i in range(int(n)):
-			#assign 1,2,3 to R,P,S
-			list.append( random.randint(1,3) )		
-		# if all of R,P,S appear or only one appears
-		if (1 in list) + (2 in list) + (3 in list) != 2 :
-			return 1 + play(n)			
-		elif not (1 in list):
-			return 1 + play( list.count(3) )		
-		elif not (2 in list):
-			return 1 + play( list.count(1) )
-		else: # 3 not in list
-			return 1 + play( list.count(2) )
-			
+def recursive_play(n):  
+    """
+    returns the number of rounds required to find the winner in a random simulation of
+    a game starting with n players. Uses recursion though so doesn't work for high n.
+    """
+    if n==1:
+        return 0    
+    else:
+        list = []
+        for i in range(int(n)):
+            #assign 1,2,3 to R,P,S
+            list.append( random.randint(1,3) )      
+        # if all of R,P,S appear or only one appears
+        if (1 in list) + (2 in list) + (3 in list) != 2 :
+            return 1 + play(n)          
+        elif not (1 in list):
+            return 1 + play( list.count(3) )        
+        elif not (2 in list):
+            return 1 + play( list.count(1) )
+        else: # 3 not in list
+            return 1 + play( list.count(2) )
+            
 def iterate(t,n):
-	"""
-	Simulates t games of n players, returning the mean number of rounds required to find
-	a winner
-	"""
-	sum = 0;
-	for i in range(t):
-		sum += play(n)
-	return sum/float(t)
-			
+    """
+    Simulates t games of n players, returning the mean number of rounds required to find
+    a winner
+    """
+    sum = 0;
+    for i in range(t):
+        sum += play(n)
+    return sum/float(t)
+            
 n = int(raw_input("Enter the number of players in the RPS game: "))
 print ("Expected number of rounds to find a winner: " + str(expected(n)) )
 t = int(raw_input("Enter number of trial games to randomly run: "))
 print ("Average number of rounds to find a winner from " + str(t) + " trials: " + str (iterate(t,n)) +"\n")
-			
+            
